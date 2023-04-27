919c336ff1fbcf61650e09b0eedcc6a72bf48f2b
Steve Molitor <steve.molitor@zapier.com>
2023-04-27 07:07:58 -0500

    Remove commented out code from robby-utils.el

--- robby-utils.el
+++ robby-utils.el
@@ -12,8 +12,6 @@
   "Replace % with %% in TEXT to avoid format string errors calling `message."
   (replace-regexp-in-string "%" "%%" text))
 
-;; (defun robby--plist-to-alist (plist))
-
 (defun robby--plist-to-alist (plist)
   "Convert PLIST to an association list (alist)."
   (cl-loop for (key value . rest) on plist by 'cddr
